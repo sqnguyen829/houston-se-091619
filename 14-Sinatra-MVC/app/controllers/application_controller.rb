6e724aad255699014e9892f49f5db266aa1ba19b
vidhisharma3193 <vidhisharma3193@gmail.com>
2019-10-07 11:14:26 -0500

    middle lecture

--- 14-Sinatra-MVC/app/controllers/application_controller.rb
+++ 14-Sinatra-MVC/app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < Sinatra::Base
 
+    set :views, "app/views"
+
+    get '/' do
+        "Hello World"
+    end
+
+    get '/home' do
+        erb :home
+    end
+
+    get '/books' do
+        @books = Book.all
+        erb :index
+    end
+
+    get '/books/:id' do 
+        # binding.pry
+        @book = Book.find(params[:id])
+        erb :show
+    end
+
 end
